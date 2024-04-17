d784962f226b7ec4a062ac8ef699b8c5033546a8
defnot001 <marc.miller994@gmail.com>
2024-04-17 18:02:16 +0200

    fix: remove unnecessary file

--- src/environment.d.ts
+++
@@ -1,9 +0,0 @@
-namespace NodeJS {
-  interface ProcessEnv {
-    APPLICATION_ID: string;
-    DISCORD_TOKEN: string;
-    PUBLIC_KEY: string;
-    GITHUB_SECRET: string;
-    DISCORD_WEBHOOK: string;
-  }
-}
