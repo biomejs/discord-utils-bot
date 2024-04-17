785892db4cd2c098a952021cac2622dccc534ef4
defnot001 <marc.miller994@gmail.com>
2024-04-17 16:22:07 +0200

    feat: qol changes for autocomplete and env management

---
+++ src/environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    APPLICATION_ID: string;
+    DISCORD_TOKEN: string;
+    PUBLIC_KEY: string;
+    GITHUB_SECRET: string;
+    DISCORD_WEBHOOK: string;
+  }
+}
