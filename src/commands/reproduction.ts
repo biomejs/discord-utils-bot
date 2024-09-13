6dbfc69ad1c06b0039ee9d656effba3331a6ac59
Nicolas Hedger <nicolas@hedger.ch>
2024-09-13 10:19:00 +0200

    remove newlines in middle of sentences

--- src/commands/reproduction.ts
+++ src/commands/reproduction.ts
@@ -8,24 +8,18 @@ export async function onReproductionSlashCommand(interaction: APIApplicationComm
     content: dedent`
     🙏 **Please provide a minimal reproduction**
 
-    If possible, please provide a link to a minimal reproduction of your issue. You can either use our
-    playground or create a minimal reproduction repository.
+    If possible, please provide a link to a minimal reproduction of your issue. You can either use our playground or create a minimal reproduction repository.
 
     - [Create a reproduction on the Playground](<https://biomejs.dev/playground/>)
     - Create a reproduction repository: \`npm create @biomejs/biome-reproduction\`
 
     **Why it's important**
 
-    Providing a minimal reproduction is important because it helps us quickly understand and diagnose the 
-    issue by isolating the problem to its core components. This makes the debugging process  faster, reduces 
-    misunderstandings, and ensures the issue can be addressed efficiently.
+    Providing a minimal reproduction is important because it helps us quickly understand and diagnose the issue by isolating the problem to its core components. This makes the debugging process  faster, reduces misunderstandings, and ensures the issue can be addressed efficiently.
 
     **What is a good minimal reproduction**
 
-    A good minimal reproduction is a simplified version of your issue that includes only the necessary code 
-    or steps to reproduce the problem. It removes any unrelated features or complexity, focusing solely on 
-    the core issue. This makes it easier for us to quickly identify and fix the problem without unnecessary 
-    distractions.
+    A good minimal reproduction is a simplified version of your issue that includes only the necessary code or steps to reproduce the problem. It removes any unrelated features or complexity, focusing solely on the core issue. This makes it easier for us to quickly identify and fix the problem without unnecessary distractions.
     `,
   });
 }
